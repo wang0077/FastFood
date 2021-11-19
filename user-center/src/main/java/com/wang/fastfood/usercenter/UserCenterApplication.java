fce03e280de0c938ad759f45f2f3d114d0928dce
wang0077 <228913299@qq.com>
2021-11-20 00:30:37 +0800

    Initial commit

---
+++ user-center/src/main/java/com/wang/fastfood/usercenter/UserCenterApplication.java
@@ -0,0 +1,13 @@
+package com.wang.fastfood.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
